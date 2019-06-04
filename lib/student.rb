45016a77be44cb39b7395ec58d5ab8cf5608f5d0
Irina Kagan <irinagamb@gmail.com>
2019-06-04 21:10:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -69,7 +69,7 @@ def self.first_x_students_in_grade_10(x)
     WHERE grade = 10
     LIMIT X
     SQL
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end
 end
