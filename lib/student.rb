85eefa69745aad26928bbaae9aedf7e6d4505d7f
Irina Kagan <irinagamb@gmail.com>
2019-06-04 21:26:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -62,14 +62,14 @@ new_student
 end
 
 
-def self.first_X_students_in_grade_10
+def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
     SELECT *
     FROM students
     WHERE grade = 10
-    LIMIT X
+    LIMIT ?
     SQL
-    DB[:conn].execute(sql).collect do |row|
+    DB[:conn].execute(sql,x).collect do |row|
       self.new_from_db(row)
     end
 end
